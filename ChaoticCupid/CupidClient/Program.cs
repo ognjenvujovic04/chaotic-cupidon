796282fd9193a6e04f792a5be949d3ffe22ff89c
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2026-06-01 18:38:58 +0200

    feat: Add CupidClient that sends love letters every 60 seconds

--- ChaoticCupid/CupidClient/Program.cs
+++ ChaoticCupid/CupidClient/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace CupidClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7128/cupidHub")
+                .Build();
+
+            await connection.StartAsync();
+            Console.WriteLine("[CUPID] Connected! Sending love letters every 60 seconds...\n");
+
+            while (true)
+            {
+                await connection.InvokeAsync("SendLoveLetters");
+                Console.WriteLine($"[CUPID] Love letters sent at {DateTime.Now}");
+                await Task.Delay(60000);
+            }
+        }
+    }
+}
